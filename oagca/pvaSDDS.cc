c34071a1a8b5f9ac54dd08ab631004f52abc8924
Robert Soliday <soliday@Macmini.attlocal.net>
2025-05-22 12:35:13 -0500

    Updated to fix issues on MacOS

--- oagca/pvaSDDS.cc
+++ oagca/pvaSDDS.cc
@@ -1142,7 +1142,7 @@ long PrepPut(PVA_OVERALL *pva, long index, double value) {
       //if (pva->pvaData[index].putData[0].stringValues[0])
       //free(pva->pvaData[index].putData[0].stringValues[0]);
     }
-    sprintf(buffer, "%lf", value);
+    snprintf(buffer, sizeof(buffer), "%lf", value);
     pva->pvaData[index].putData[0].stringValues[0] = (char *)malloc(sizeof(char) * (strlen(buffer) + 1));
     strcpy(pva->pvaData[index].putData[0].stringValues[0], buffer);
   }
@@ -1189,7 +1189,7 @@ long PrepPut(PVA_OVERALL *pva, long index, double *value, long length) {
       pva->pvaData[index].putData[0].stringValues = (char **)malloc(sizeof(char *) * length);
     }
     for (i = 0; i < length; i++) {
-      sprintf(buffer, "%lf", value[i]);
+      snprintf(buffer, sizeof(buffer), "%lf", value[i]);
       pva->pvaData[index].putData[0].stringValues[i] = (char *)malloc(sizeof(char) * (strlen(buffer) + 1));
       strcpy(pva->pvaData[index].putData[0].stringValues[i], buffer);
     }
@@ -1212,7 +1212,7 @@ long PrepPut(PVA_OVERALL *pva, long index, int64_t value) {
       //if (pva->pvaData[index].putData[0].stringValues[0])
       //free(pva->pvaData[index].putData[0].stringValues[0]);
     }
-    sprintf(buffer, "%ld", value);
+    snprintf(buffer, sizeof(buffer), "%lld", value);
     pva->pvaData[index].putData[0].stringValues[0] = (char *)malloc(sizeof(char) * (strlen(buffer) + 1));
     strcpy(pva->pvaData[index].putData[0].stringValues[0], buffer);
   }
@@ -1259,7 +1259,7 @@ long PrepPut(PVA_OVERALL *pva, long index, int64_t *value, long length) {
       pva->pvaData[index].putData[0].stringValues = (char **)malloc(sizeof(char *) * length);
     }
     for (i = 0; i < length; i++) {
-      sprintf(buffer, "%ld", value[i]);
+      snprintf(buffer, sizeof(buffer), "%lld", value[i]);
       pva->pvaData[index].putData[0].stringValues[i] = (char *)malloc(sizeof(char) * (strlen(buffer) + 1));
       strcpy(pva->pvaData[index].putData[0].stringValues[i], buffer);
     }
